d17bb38552fc897b272b8b3aea72cd2482c3e25b
gunt <4261629+gunt@users.noreply.github.com>
2019-08-28 09:54:11 -0600

    removing modal function merge showDetails

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,11 @@
         var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=50';
 
 
-        // Ajax method jQuery
         function loadList() {
-            return $.ajax(apiUrl, {dataType:'json'}).then(function (item) {
-                $.each(item.results, function (index, item) {
+            return $.ajax(apiUrl, {
+                dataType: 'json'
+            }).then(function (item) {
+                $.each(item.results, function (_index, item) {
                     var pokemon = {
                         name: item.name,
                         detailsUrl: item.url
@@ -22,8 +23,9 @@
 
         function loadDetails(item) {
             var url = item.detailsUrl;
-            return $.ajax(url, {dataType:'json'}).then(function (details) {
-                // Now we add the details to the item
+            return $.ajax(url, {
+                dataType: 'json'
+            }).then(function (details) {
                 item.imageUrl = details.sprites.front_default;
                 item.height = details.height;
                 item.types = Object.keys(details.types);
@@ -32,14 +34,13 @@
             });
         }
 
-        // if not an object // not display
+        
         function add(pokemon) {
-            repository.push(pokemon);
-            // if (typeof repository === 'object') {
-            //     repository.push(pokemon)
-            // } else {
-            //     console.error('Is not an Object');
-            // }
+            if (typeof repository === 'object') {
+                repository.push(pokemon)
+            } else {
+                console.error('Is not an Object');
+            }
         }
 
         function getAll() {
@@ -54,41 +55,24 @@
         };
     })();
 
-    function showModal(item) {
-        var $modalContainer = $('.modal-container');
-        $modalContainer.empty();
-        
-        $modal = $('<div class = "modal-body"></div>');
-
-        $nameElement = $('<h1 class="modal-title" id="ModalLabel">' + item.name + '</h1>');
-
-        // pokemon image class fixed it - not recognize pokemon as item
-        $imageElement = $('<img class = "pokemonimage">');
-        $imageElement.attr('src', item.imageUrl);
-
-        $typesElement = $('<p>' + 'height : ' + item.height + '</p>');
-
-        //appending 
-        // $modal.append($closeButtonElement);
-        $modal.append($nameElement);
-        $modal.append($imageElement);
-        $modal.append($typesElement);
-        $modalContainer.append($modal);
-        // $modalContainer.addClass('is-visible');
-    }
 
     function addListItem(pokemon) {
-        $listItemElement = $('<div class="pokemon-list__item"></div>');
+        $listItemElement = $('<div class="list-group-item list-group-item-action"></div>');
         $pokemonList = $('.pokemon-list');
-        $pokemonName = $('<span></span>');
-        $button = $('<button type ="button" class= "btn btn-primary" data-toggle="modal" data-target="#modalContent"</button>');
+        $pokemonName = $('<h1 class="row justify-content-center" </h1>');
+        $button = $('<button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#modalContent"></button>');
+        $pokemonName.text(pokemon.name);
 
-        // Append
+
+//         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+//   Launch demo modal
+// </button>
+
+         // Append
         $listItemElement.append($button);
         $listItemElement.append($pokemonName);
-        $pokemonName.text(pokemon.name);
         $pokemonList.append($listItemElement);
-        $button.on('click', function () {
+        $button.click(function () {
             showDetails(pokemon);
         });
     }
@@ -96,20 +80,29 @@
     //calling details of the pokemons
     function showDetails(item) {
         pokemonRepository.loadDetails(item).then(function () {
-            //displays details in a modal
-            showModal(item);
+            
+            var $modalContainer = $('.modalContainer');
+            $modalContainer.empty();
+
+            $modal = $('<div class = "modal-body"></div>');
+
+            $nameElement = $('<h1 class="modal-title" id="ModalLabel">' + item.name + '</h1>');
+
+            $imageElement = $('<img class = "pokemonimage">');
+            $imageElement.attr('src', item.imageUrl);
+
+            $typesElement = $('<p>' + 'height : ' + item.height + '</p>');
+
+            $modal.append($nameElement);
+            $modal.append($imageElement);
+            $modal.append($typesElement);
+            $modalContainer.append($modal);
         });
     }
 
-
-    //clearing all existing modal content  // jQuery major attention on classes
-
-    
-
-
     pokemonRepository.loadList().then(function () {
         // Now the data is loaded!
-        $.each(pokemonRepository.getAll(), function (index, pokemon) {
+        $.each(pokemonRepository.getAll(), function (_index, pokemon) {
             addListItem(pokemon);
         });
     });
