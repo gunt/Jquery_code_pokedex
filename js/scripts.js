23a89c291389d47038973eed98784ca5bc199955
gunt <4261629+gunt@users.noreply.github.com>
2019-08-27 18:07:21 -0600

    reorganize update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,6 @@
             });
         }
 
-
         // if not an object // not display
         function add(pokemon) {
             if (typeof repository === 'object') {
@@ -40,22 +39,38 @@
             } else {
                 console.error('Is not an Object');
             }
-
         }
 
         function getAll() {
             return repository;
         }
 
+        return {
+            add: add,
+            getAll: getAll,
+            //addListItem: addListItem,
+            //showDetails: showDetails,
+            loadList: loadList,
+            loadDetails: loadDetails,
+            //showModal: showModal,
+            //hideModal: hideModal
+        };
+    })();
+
         function addListItem(pokemon) {
+            
+            // $listItemElement = $('<listItemElement class = "pokemon-list__item">' + '<li>');
             $listItemElement = $('<listItemElement class = "pokemon-list__item">' + '<li>');
-            $('ul').append($listItemElement);
+            
+            $pokemonList = $('.pokemon-list');
+            $pokemonName = $('<span')
+            //$button = $('<button type = "button">' + pokemon.name + '</button>');
+            $button = $('<button type ="button" class= "button" data-toggle="modal" data-target="#modalContent"</button>');
 
-            // !important/ atention to learn purpose // caused major confusion in structure index.html review
-            $button = $(
-                '<button class = "button">' + pokemon.name + '</button>'
-            );
 
+            
+
+            
             $listItemElement.append($button);
             $button.on('click', function () {
                 showDetails(pokemon);
@@ -71,17 +86,12 @@
         }
 
 
-        
-
-        
-
-        
-//clearing all existing modal content  // jQuery major attention on classes
+        //clearing all existing modal content  // jQuery major attention on classes
 
         function showModal(item) {
             var $modalContainer =
 
-            $('#modal-container').text('');
+                $('#modal-container').text('');
             $modal = $('<div class = "modal">');
 
             //close button
@@ -102,38 +112,26 @@
             $modal.append($imageElement);
             $modal.append($typesElement);
             $modalContainer.append($modal);
-            $modalContainer.addClass('is-visible'); 
+            $modalContainer.addClass('is-visible');
         }
 
-        function hideModal() {
-            $('#modal-container').removeClass('is-visible');
-        }
+        // Bootstrap Modal instead
+        // function hideModal() {
+        //     $('#modal-container').removeClass('is-visible');
+        // }
 
-        $(document).on('keydown', function (event) {
-            if (event.key === 'Escape' && $('#modal-container').hasClass('is-visible')) {
-                hideModal();
-            }
-        });
+        // $(document).on('keydown', function (event) {
+        //     if (event.key === 'Escape' && $('#modal-container').hasClass('is-visible')) {
+        //         hideModal();
+        //     }
+        // });
 
         // $('#modal-container').on('click', function (event) {
+        //     if ($(event.target).closest('#modal-container').length) {
+        //         hideModal();
+        //     }
+        // });
 
-        $('#modal-container').on('click', function (event) {
-            if ($(event.target).closest('#modal-container').length) {
-                hideModal();
-            }
-        });
-
-        return {
-            add: add,
-            getAll: getAll,
-            addListItem: addListItem,
-            showDetails: showDetails,
-            loadList: loadList,
-            loadDetails: loadDetails,
-            showModal: showModal,
-            hideModal: hideModal
-        };
-    })();
 
     pokemonRepository.loadList().then(function () {
         // Now the data is loaded!
