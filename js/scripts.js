c8ac59a3b54edc6f284562da5bbaf9290227dd70
gunt <4261629+gunt@users.noreply.github.com>
2019-08-28 10:25:54 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -63,11 +63,6 @@
         $button = $('<button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#modalContent"></button>');
         $pokemonName.text(pokemon.name);
 
-
-//         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-//   Launch demo modal
-// </button>
-
          // Append
         $listItemElement.append($button);
         $listItemElement.append($pokemonName);
